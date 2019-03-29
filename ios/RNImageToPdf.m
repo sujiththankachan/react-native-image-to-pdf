5a20a03523df80e27173b433ce91d5bcc38f7f4d
Daniel Frenberg <daniel.frenberg@fortnox.se>
2019-03-29 16:36:02 +0100

    WIP Reduce size of images on ios

--- ios/RNImageToPdf.m
+++ ios/RNImageToPdf.m
@@ -38,13 +38,37 @@ RCT_EXPORT_METHOD(createPDFbyImages:(NSDictionary *)options
     
     for (NSString *imagePath in imagePathArray) {
         UIImage *img = [UIImage imageWithContentsOfFile:imagePath];
-        UIImageView *imageView = [[UIImageView alloc]initWithImage:img];
+        UIImage *rszdImg = [self compress:[self imageWithImage:img scaledToSize:[self calculateSize:img]]];
+        
+        UIImageView *imageView = [[UIImageView alloc]initWithImage:rszdImg];
         [self.imageViewArray addObject:imageView];
     }
     
     [self createPDFWithFilename:filename];
     NSLog(@"PDF was created successfully");
 }
+
+- (CGSize)calculateSize:(UIImage *)image {
+    CGSize size = [image size];
+    if (size.width < 1080) return size;
+    float ar = size.height/size.width;
+    float width = 1080;
+    float height = 1080 * ar;
+    return CGSizeMake(width, height);
+}
+
+- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
+    UIGraphicsBeginImageContext(newSize);
+    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (UIImage *)compress:(UIImage *)image {
+    return [UIImage imageWithData:UIImageJPEGRepresentation(image, 0.8)];
+}
+
 
 - (void)createPDFWithFilename:(NSString *)filename {
     
